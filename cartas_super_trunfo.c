f2d9dba21ff708c819b28d6cb8901928ed27cf39
Mageske <mageskeesantana@gmail.com>
2026-03-17 00:19:06 -0300

    Conclui o Desafio no Nivel Aventureiro.

--- cartas_super_trunfo.c
+++ cartas_super_trunfo.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int main() {
+    
     // Jogador 1 //
-    char codigo1[50], estado1[50], cidade1[50];
+     char codigo1[50], estado1[50], cidade1[50];
     int populacao1, pontos1;
     float area1, pib1;
+    float pibpercapta1, densidade1;  
+    
     // Jogador 2 //
     char estado2[50], codigo2[50], cidade2[50];
     int populacao2, pontos2;
     float area2, pib2;
+    float pibpercapta2, densidade2;
+    
     //Leitura Jogador 1 //
     printf("            Jogador 1 \n");
     printf("Estado: "); scanf(" %s", estado1);
@@ -17,6 +22,7 @@ int main() {
     printf("Área: "); scanf(" %f", &area1);
     printf("Pib: "); scanf(" %f", &pib1);
     printf("Pontos Turísticos: "); scanf(" %d", &pontos1);
+    
     //Leitura Jogador 2 //
     printf("             Jogador 2  \n");
     printf("Estado: "); scanf(" %s", estado2);
@@ -26,9 +32,18 @@ int main() {
     printf("Área: "); scanf(" %f", &area2);
     printf("Pib: "); scanf(" %f", &pib2);
     printf("Pontos Turísticos: "); scanf(" %d", &pontos2);
+    
+    //Calculando a Densidade Populacional//
+    densidade1 = populacao1 / area1;
+    densidade2 = populacao2 / area2;
+    
+    //Calculando o Pib per Capta//
+    pibpercapta1 = (pib1 * 1000000000.0) / populacao1;
+    pibpercapta2 = (pib2 * 1000000000.0) / populacao2;
+   
     //Informações dos Jogadores//
-    printf("\nCarta 1:\n Estado: %s\n Código: %s\n Cidade: %s\n População: %d\n Área: %.2f km2\n Pib: %.2f bilhões de reais\n Pontos Turisticos: %d\n", estado1, codigo1, cidade1, populacao1, area1, pib1, pontos1);
-     printf("\nCarta 2:\n Estado: %s\n Código: %s\n Cidade: %s\n População: %d\n Área: %.2f km2\n Pib: %.2f bilhões de reais\n Pontos Turisticos: %d\n", estado2, codigo2, cidade2, populacao2, area2, pib2, pontos2);
+    printf("\nCarta 1:\n Estado: %s\n Código: %s\n Cidade: %s\n População: %d\n Área: %.2f km2\n Pib: %.2f bilhões de reais\n Pontos Turisticos: %d\n Densidade Populacional: %.2f hab/km2\n Pib per Capta: %.2f reais\n", estado1, codigo1, cidade1, populacao1, area1, pib1, pontos1, densidade1, pibpercapta1);
+     printf("\nCarta 2:\n Estado: %s\n Código: %s\n Cidade: %s\n População: %d\n Área: %.2f km2\n Pib: %.2f bilhões de reais\n Pontos Turisticos: %d\n Densidade Populacional: %.2f hab/km2\n Pib per Capta: %.2f reais\n", estado2, codigo2, cidade2, populacao2, area2, pib2, pontos2, densidade2, pibpercapta2);
      
      return 0;
     
